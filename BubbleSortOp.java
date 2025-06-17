b649ec05c562f2f96560537b653be1561bcd0d47
Mulla Mahammad Sohail <sohailmulla767@gmail.com>
2025-06-17 10:20:31 +0530

    Add files via upload

--- BubbleSortOp.java
+++ BubbleSortOp.java
@@ -1,28 +1,30 @@
-public class BubbleSort{
-	public static int bubbleSort(int[] arr){
+public class BubbleSortOp{
+	public static void bubbleSort(int[] arr){
 		int n=arr.length;
-		int swapcount=0;
+		boolean swapped=false;
 		for(int i=0;i<n-1;i++){
 			for(int j=0;j<n-i-1;j++){
 				if(arr[j]>arr[j+1]){
 					int temp=arr[j];
 					arr[j]=arr[j+1];
 					arr[j+1]=temp;
-					swapcount++;
+					swapped=true;
 				}
 			}
+			if(!swapped){
+				System.out.println("list is sorted,Loop Breaked!! ");
+				break;
+			}
 		}
-		return swapcount;
 	}
 	public static void main(String[] args){
-		int[] arr={5,1,4,3,6,2};
+		int[] arr={1,2,3,4,5,6};
 		System.out.println("Before sorting:");
 		for(int num:arr)
 			System.out.print(num+" ");
 		System.out.println("\nAfter sorting:");
-		int count=bubbleSort(arr);
+		bubbleSort(arr);
 		for(int num:arr)
 			System.out.print(num+" ");
-		System.out.println("\nSwap Count:"+count);
 	}
 }
